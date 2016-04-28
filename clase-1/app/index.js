04fc0e96a6c5bcbdc6162464257a10a340001e92
Sergio Rafael Gianazza <sgianazza@gmail.com>
2016-04-28 10:45:17 -0300

    Agregado resolución a agregar elementos al DOM sin React

--- clase-1/app/index.js
+++ clase-1/app/index.js
@@ -1,1 +1,11 @@
-console.log(" Hola Mundo! ");
+const paragraphs = document.createElement('div');
+const text1 = document.createTextNode('Hola Mundo');
+const text2 = document.createTextNode('Yo soy Sergio');
+
+paragraphs.appendChild(text1);
+paragraphs.appendChild(document.createElement('br'));
+paragraphs.appendChild(text2);
+
+const content = document.getElementById('content');
+
+content.appendChild(paragraphs);
