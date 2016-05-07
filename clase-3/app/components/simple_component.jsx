2a812eae6fdd92f012c13ca549997c5b29e14a4e
Sergio Rafael Gianazza <sgianazza@gmail.com>
2016-05-07 20:19:26 -0300

    ejercicio 1 clase 3 resuelto

---
+++ clase-3/app/components/simple_component.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SimpleComponent extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Este texto desaparecerá en 10 segundos!</h1>
+      </div>
+    );
+  }
+}
