ae4b1c1e9d372d6e9eda58f663039cd890691ebf
Sergio Rafael Gianazza <sgianazza@gmail.com>
2016-05-07 13:12:01 -0300

    resolución primer ejercicio

---
+++ clase-2/app/components/simple_component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class SimpleComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Mi primer componente React</h1>
+        <p>Este componente es la demostración que aprendimos React, React-DOM y JSX!</p>
+      </div>
+    );
+  }
+}
--- clase-2/app/index.js
+++ clase-2/app/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SimpleComponent from './components/simple_component';
 
 const content = document.getElementById('content');
 
-ReactDOM.render(<div>
-        <span>Hola Mundo</span><br />
-        <span>Soy Sergio</span>
-      </div>
-, content);
+ReactDOM.render(<SimpleComponent />, content);
