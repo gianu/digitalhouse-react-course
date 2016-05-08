0a58c49e57ae14da9d386fd3fd03869041e3644a
Sergio Rafael Gianazza <sgianazza@gmail.com>
2016-05-08 10:34:51 -0300

    Agregada resolución al la parte 2 del ejercicio 1 de la clase 3

--- clase-3/app/components/simple_component.jsx
+++ clase-3/app/components/simple_component.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 export default class SimpleComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showText: true
+    };
+  }
+
+  renderText() {
+    if(this.state.showText) {
+      return (<p>Texto que aparece y desaparece</p>);
+    } else {
+      return null;
+    }
+  }
+
+  handleToggleText() {
+    this.setState({
+      showText: !this.state.showText
+    });
+  }
+  
   render() {
     return(
       <div>
-        <h1>Este texto desaparecerá en 10 segundos!</h1>
+        {this.renderText()}
+        <button onClick={this.handleToggleText.bind(this)}>Toggle Text</button>
       </div>
     );
   }
--- clase-3/app/index.js
+++ clase-3/app/index.js
@@ -5,7 +5,3 @@ import SimpleComponent from './components/simple_component';
 const content = document.getElementById('content');
 
 ReactDOM.render(<SimpleComponent />, content);
-
-setTimeout(() => {
-  ReactDOM.unmountComponentAtNode(content);
-}, 10000);
