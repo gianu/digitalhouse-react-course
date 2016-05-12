a5a70aa519d5e1fed2b5b2fa3c6d4f4b35c3419e
Sergio Rafael Gianazza <sgianazza@gmail.com>
2016-05-12 10:11:53 -0300

    Estructura basica para ejercicio 1 de la clase 4

---
+++ clase-4/app/components/simple_component.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SimpleComponent extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Proximamente...</h1>
+      </div>
+    );
+  }
+}
---
+++ clase-4/app/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SimpleComponent from './components/simple_component';
+
+const content = document.getElementById('content');
+
+ReactDOM.render(<SimpleComponent />, content);
