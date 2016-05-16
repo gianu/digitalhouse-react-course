6b74f5b07780d82c01d32c9560039e24c1d4998c
Sergio Rafael Gianazza <sgianazza@gmail.com>
2016-05-16 10:22:54 -0300

    Resolución ejercicio 2 clase 4

--- clase-4/app/components/simple_component.jsx
+++ clase-4/app/components/simple_component.jsx
@@ -4,7 +4,9 @@ export default class SimpleComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      controlledInputValue: ''
+      controlledInputValue: '',
+      controlledSelect: '-1',
+      radioValue: 'radio 1'
     };
   }
 
@@ -12,6 +14,8 @@ export default class SimpleComponent extends Component {
     console.log('---- VALORES ----');
     console.log(this.state.controlledInputValue);
     console.log(this._uncontrolledText.value);
+    console.log(this.state.controlledSelect)
+    console.log(this.state.radioValue);
     console.log('-----------------');
   }
 
@@ -23,6 +27,24 @@ export default class SimpleComponent extends Component {
           value={this.state.controlledInputValue}
           onChange={(evt) => this.setState({ controlledInputValue: evt.target.value })}/> <br />
         <input type='text' defaultValue='' ref={(el) => { this._uncontrolledText = el; } }/><br />
+        <select value={this.state.controlledSelect} onChange={(evt) => this.setState({ controlledSelect: evt.target.value })}>
+          <option value='-1'>Sin valor</option>
+          <option value='1'>Primer valor</option>
+          <option value='2'>Segundo valor</option>
+          <option value='3'>Terecer valor</option>
+        </select><br />
+        <input
+         type='radio'
+         name='radio 1'
+         checked={this.state.radioValue === 'radio 1'}
+         onChange={() => this.setState({radioValue: 'radio 1'})}
+        />Radio 1&nbsp;
+        <input
+          type='radio'
+          name='radio 2'
+          checked={this.state.radioValue === 'radio 2'}
+          onChange={() => this.setState({radioValue: 'radio 2'})}
+        />Radio 2<br />
         <button onClick={this.handleSubmit.bind(this)}>Enviar!</button>
       </div>
     );
