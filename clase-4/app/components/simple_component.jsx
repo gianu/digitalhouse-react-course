42e94e547a68487bb1c71044db92a8f0b588ba34
Sergio Rafael Gianazza <sgianazza@gmail.com>
2016-05-16 09:43:56 -0300

    agregada resolución al ejercicio 1 de la clase 4

--- clase-4/app/components/simple_component.jsx
+++ clase-4/app/components/simple_component.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 export default class SimpleComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      controlledInputValue: ''
+    };
+  }
+
+  handleSubmit() {
+    console.log('---- VALORES ----');
+    console.log(this.state.controlledInputValue);
+    console.log(this._uncontrolledText.value);
+    console.log('-----------------');
+  }
+
   render() {
     return(
       <div>
-        <h1>Proximamente...</h1>
+        <input
+          type='text'
+          value={this.state.controlledInputValue}
+          onChange={(evt) => this.setState({ controlledInputValue: evt.target.value })}/> <br />
+        <input type='text' defaultValue='' ref={(el) => { this._uncontrolledText = el; } }/><br />
+        <button onClick={this.handleSubmit.bind(this)}>Enviar!</button>
       </div>
     );
   }
