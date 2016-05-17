a77e7596107c306a5379d18ad336f4d823fbb202
Sergio Rafael Gianazza <sgianazza@gmail.com>
2016-05-17 09:28:46 -0300

    Agregado scaffolding para clase 5

--- clase-5/app/index.js
+++ clase-5/app/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import createLogger from 'redux-logger';
+import reducers from './reducers';
 import SimpleComponent from './components/simple_component';
+
+const logger = createLogger();
+let store = createStore(
+  reducers,
+  applyMiddleware(logger)
+);
 
 const content = document.getElementById('content');
 
-ReactDOM.render(<SimpleComponent />, content);
+render(
+  <Provider store={store}>
+    <SimpleComponent />
+  </Provider>,
+  content
+);
---
+++ clase-5/app/reducers/index.js
@@ -0,0 +1,7 @@
+const initialState = {
+  counter: 0
+};
+
+export default function counterApp(state = initialState, action) {
+  return state;
+}
