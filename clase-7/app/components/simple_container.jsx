3c3a5bb0f804a907aca512e1fced61bc1c5615dc
Sergio Rafael Gianazza <sgianazza@gmail.com>
2016-05-19 19:07:33 -0300

    Agregado base del ejercicio 1 de la clase 7

---
+++ clase-7/app/components/simple_container.jsx
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+// import { createSelector } from 'reselect';
+import SimpleComponent from './simple_component';
+
+// const SimpleContainer = connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(SimpleComponent);
+
+// export default SimpleContainer;
+
+export default SimpleComponent;
---
+++ clase-7/app/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import createLogger from 'redux-logger';
+// import reducers from './reducers';
+import SimpleContainer from './components/simple_container';
+
+// const logger = createLogger();
+// let store = createStore(
+//   reducers,
+//   applyMiddleware(logger)
+// );
+
+// render(
+//   <Provider store={store}>
+//     <SimpleContainer />
+//   </Provider>,
+//   content
+// );
+
+render (
+  <SimpleContainer />,
+  content
+);
